e5ebd30db3e0ca409fc27305268b5bd17c2a48e4
iVanGB93 <ivanguachbeltran@gmail.com>
2024-06-01 14:12:37 -0400

    latest

--- core/settings.py
+++ core/settings.py
@@ -64,14 +64,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- """
-DATABASES = {
+
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
@@ -83,7 +83,7 @@ DATABASES = {
             'connect_timeout': 10,  # timeout in seconds
         },
     }
-}
+} """
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
